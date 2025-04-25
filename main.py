8f96f8a232088bbc0e236be25f490788af443796
Jezura <169179776+Jezura12867@users.noreply.github.com>
2025-04-25 20:57:06 +0200

    2nd day commit
    -Added much better parsing ability, however, the code is still not able to read and execute the input

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
 import turtle
 
-copy_this = """1) AB; |AB| = 5 cm
-2) k; k(A; 5 cm)
-END"""
+#1) AB; |AB| = 5 cm
+#2) k; k(A; 5 cm)
+#END
 
+# Global vars:
 direct_input = ""
+prev_i = ""
 inp = []
 out = []
 
-while direct_input != "END":
-    direct_input = input()
-    print(direct_input)
-    if ")" in direct_input:
-        continued_inp = ""
-        for i in direct_input:
-            if i == ")":
-                continue
-            try:
-                i = int(i)
-                continue
-            except:
-                pass
-            continued_inp += i
-        print(continued_inp)
-                
+# Parses  ⊙.⊙
+class Parser:
+
+    # Determines if input (num) is an int
+    def is_number(num):
+        try:
+            num = int(num)
+            return True
+                                            
+        except:
+            return False
+        
+    #Repeats until you type "END"
+    while direct_input != "END":
+    #Takes your input
+        direct_input = input()
+
+        # Removes closing brackets
+        if ")" in direct_input:
+            continued_inp = ""
+            for i in direct_input:
+
+                if direct_input[0] == i and is_number(i) == True:
+                    prev_i = i
+                    continue
+                                    
+                elif is_number(prev_i) == True and i == ")":
+                    prev_i = i
+                    continue
+
+                prev_i = i
+                continued_inp += i
+                    
         inp.append(continued_inp)
+    inp.pop(len(inp) - 1)
 
 
 print(inp)
